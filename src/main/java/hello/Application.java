6622908d33aec30f5efe12267f5e151f60f8d521
Konrad Sochacki <sochacki.konrad@gmail.com>
2019-12-16 10:37:11 +0100

    Correct booking service export

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -3,19 +3,29 @@ package hello;
 import hello.service.CabBookingService;
 import hello.service.CabBookingServiceImpl;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.remoting.caucho.HessianServiceExporter;
 import org.springframework.remoting.httpinvoker.HttpInvokerServiceExporter;
+import org.springframework.remoting.support.RemoteExporter;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class Application {
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean(name = "/booking") HttpInvokerServiceExporter accountService() {
-        HttpInvokerServiceExporter exporter = new HttpInvokerServiceExporter();
+    @Bean(name = "/booking")
+    RemoteExporter bookingService() {
+        HessianServiceExporter exporter = new HessianServiceExporter();
         exporter.setService( new CabBookingServiceImpl() );
         exporter.setServiceInterface( CabBookingService.class );
         return exporter;
